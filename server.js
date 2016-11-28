d68309b3bdf5f427edcc82d4d0238f70a91cb360
Hoffmast01 <hoffmast@oregonstate.edu>
2016-11-28 14:10:29 -0800

    set path

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 
+
 var app = express();
 var port = process.env.PORT || 3000;
 
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + '/public'));
+app.use("/styles", express.static(__dirname + 'public/styles'));
+app.use("/index", express.static(__dirname + 'public/index'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
