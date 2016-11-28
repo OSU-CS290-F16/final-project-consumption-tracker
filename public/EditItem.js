0c5e104999297fc8f4e60c31ee99bf98b4f136aa
Hoffmast01 <hoffmast@oregonstate.edu>
2016-11-28 13:51:32 -0800

    Edit item function framework

---
+++ public/EditItem.js
@@ -0,0 +1,10 @@
+//Edit existing item or delete if amount left is 0
+
+function editObject(var item, var newAmount)
+{
+  //go into item set amount to new amount
+
+  //if item.amount == 0, delete item data
+
+  return item;
+}
