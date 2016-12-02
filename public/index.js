f6667e7cf6267860a9f9a35f9aa700558ab65237
Hoffmast01 <hoffmast@oregonstate.edu>
2016-12-02 13:47:45 -0800

    Most of the logic for client side js
    just missing add item function, and delete item

--- public/index.js
+++ public/index.js
@@ -1,14 +1,21 @@
 function displayModal()
 {
-  
+  var historyModal = document.getElementById('modal');
+  historyModal.classList.remove('hidden');
 }
 function closeModal()
 {
+  var historyModal = document.getElementById('modal');
+  historyModal.classList.add('hidden');
   clearModalValue();
 }
 function clearModalValue()
 {
-
+  var modalInputs = document.getElementsByClassName('modal-input-element');
+  for (var i = 0; i < modalInputs.length; i++) {
+    var input = modalInputs[i].querySelector('input, textarea');
+    input.value = '';
+  }
 }
 function insertNewItem()
 {
--- public/tracker.js
+++ public/tracker.js
@@ -1,27 +1,45 @@
-function displayNewHistory()
+function displayNewHistory() //displays new history form
 {
-
+  var historyModal = document.getElementById('history-modal');
+  historyModal.classList.remove('hidden');
 }
-function closeNewHistoryModal()
+function closeNewHistoryModal() // closes new history form
 {
-
+  var historyModal = document.getElementById('history-modal');
+  historyModal.classList.add('hidden');
+  clearNewHistoryModalValues();
 }
-function clearNewHistoryModalValues()
+function clearNewHistoryModalValues() // clears history forms values
 {
-
+  var historyFields = document.getElementsByClassName('history-input-element');
+  for (var i = 0; i < historyFields.length; i++) {
+    var input = historyFields[i].querySelector('input, textarea');
+    input.value = '';
+  }
 }
 function addNewHistory()
 {
 
 }
-
 function displayEditHistoryModal()
 {
+  var editHistoryModal = document.getElementById('edit-history-modal');
+  historyModal.classList.remove('hidden');
 
 }
 function closeEditHistoryModal()
 {
-
+  var editHistoryModal = document.getElementById('edit-history-modal');
+  historyModal.classList.add('hidden');
+  clearEditHistory();
+}
+function clearEditHistory()
+{
+  var editHistoryFields = document.getElementsByClassName('edit-history-input-element');
+  for (var i = 0; i < editHistoryFields.length; i++) {
+    var input = editHistoryFields[i].querySelector('input, textarea');
+    input.value = '';
+  }
 }
 function changeHistoryItem()
 {
